d6f67c5583f03c711d7fbca295e4a88181e8853f
Lee hyemi <107706338+hammimo@users.noreply.github.com>
2022-11-24 14:34:28 +0900

    개선하기 😎
    무조건 다시 복습 ✏️

--- main.js
+++ main.js
@@ -19,37 +19,48 @@ function onAdd(){
     input.value = '';
     input.focus();
 }
-
+let id = 0; //실제로는 UUID를 쓰는 것이 좋다.
 function createItem(text){
     const itemRow = document.createElement('li');
     itemRow.setAttribute('class', 'item__row');
+    itemRow.setAttribute('data-id', id);
+    itemRow.innerHTML = `
+    <div class="item">
+        <span class="item__name">${text}</span>
+            <button class="item__delete">
+                <i class ="fa-solid fa-trash-can" data-id=${id}></i>
+            </button>
+    </div>
+    <div class="item__divider"></div> 
+    `;
+    id++;
 
-    const item = document.createElement('div');
-    item.setAttribute('class', 'item');
+    // const item = document.createElement('div');
+    // item.setAttribute('class', 'item');
 
-    const name = document.createElement('span');
-    name.setAttribute('class', 'item__name');
-    name.innerText = text; 
-    // input에서 받은 text를 받아야 하므로 innerText!
+    // const name = document.createElement('span');
+    // name.setAttribute('class', 'item__name');
+    // name.innerText = text; 
+    // // input에서 받은 text를 받아야 하므로 innerText!
 
-    const deleteBtn = document.createElement('button');
-    deleteBtn.setAttribute('class', 'item__delete');
-    deleteBtn.innerHTML = '<i class="fa-solid fa-trash-can"></i>';
-    // delete button은 변경될 이유가 없으므로 복붙^^;
-    deleteBtn.addEventListener('click', ()=>{
-        items.removeChild(itemRow)});
+    // const deleteBtn = document.createElement('button');
+    // deleteBtn.setAttribute('class', 'item__delete');
+    // deleteBtn.innerHTML = '<i class="fa-solid fa-trash-can"></i>';
+    // // delete button은 변경될 이유가 없으므로 복붙^^;
+    // deleteBtn.addEventListener('click', ()=>{
+    //     items.removeChild(itemRow)});
 
 
-    const itemDivider = document.createElement('div');
-    itemDivider.setAttribute('class', 'item__divider');
+    // const itemDivider = document.createElement('div');
+    // itemDivider.setAttribute('class', 'item__divider');
 
-    // item 에 span과 deletebutton을 넣어줘야 한다.
-    item.appendChild(name);
-    item.appendChild(deleteBtn);
+    // // item 에 span과 deletebutton을 넣어줘야 한다.
+    // item.appendChild(name);
+    // item.appendChild(deleteBtn);
 
-    // itemRow에 생성한 item과 divider 추가
-    itemRow.appendChild(item);
-    itemRow.appendChild(itemDivider);
+    // // itemRow에 생성한 item과 divider 추가
+    // itemRow.appendChild(item);
+    // itemRow.appendChild(itemDivider);
 
     return itemRow;
     };
@@ -64,4 +75,13 @@ input.addEventListener('keypress', (event) => { // enter를 쳐도 입력 가능
  if(event.key === 'Enter'){
     onAdd();
  }
+});
+
+items.addEventListener('click', event => {
+    const id = event.target.dataset.id;
+    if(id){
+        const toBeDeleted = document.querySelector(`.item__row[data-id="${id}"]`);
+        toBeDeleted.remove();
+    }
+    
 });
